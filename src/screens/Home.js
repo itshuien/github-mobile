a5f93d0bfa674bfaf24d03e8b5d7b73da2a1d7a7
itshuien <huien_wong@hotmail.com>
2021-02-23 10:38:13 +0800

    Dismiss keyboard onPress elsewhere

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -32,32 +32,36 @@ export default function Home({ navigation }) {
   }, [searchKeyword]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.header}>
-        <Image source={{ uri: organization.avatar_url }} style={styles.avatar} />
-        <View style={{ flex: 1 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 2, }}>{organization.name}</Text>
-          <Text style={{ fontSize: 12 }}>{organization.description}</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.header}>
+          <Image source={{ uri: organization.avatar_url }} style={styles.avatar} />
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 2, }}>{organization.name}</Text>
+            <Text style={{ fontSize: 12 }}>{organization.description}</Text>
+          </View>
         </View>
-      </View>
+        
 
-      <SearchInput searchKeyword={searchKeyword} onChangeText={value => setSearchKeyword(value)} />
+        <SearchInput searchKeyword={searchKeyword} onChangeText={value => setSearchKeyword(value)} />
 
-      {searchKeyword.length ? <Text style={styles.searchResultInfo}>{filteredRepos.length} repositories found</Text> : null}
+        {searchKeyword.length ? <Text style={styles.searchResultInfo}>{filteredRepos.length} repositories found</Text> : null}
 
-      <FlatList
-        data={filteredRepos}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => navigation.navigate('Repository', { repository: item })} style={styles.listItem}>
-            <Text style={{ fontWeight: '700', marginBottom: 4 }}>{item.name}</Text>
-            <Text style={{ fontSize: 12 }}>{item.description}</Text>
-            {item.language ? <LanguageLabel language={item.language} /> : null}
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item.id.toString()}
-        style={styles.flatList}
-      />
-    </SafeAreaView>
+        <FlatList
+          data={filteredRepos}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => navigation.navigate('Repository', { repository: item })} style={styles.listItem}>
+              <Text style={{ fontWeight: '700', marginBottom: 4 }}>{item.name}</Text>
+              <Text style={{ fontSize: 12 }}>{item.description}</Text>
+              {item.language ? <LanguageLabel language={item.language} /> : null}
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item.id.toString()}
+          style={styles.flatList}
+          contentContainerStyle={{ paddingBottom: 24 }}
+        />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
 
