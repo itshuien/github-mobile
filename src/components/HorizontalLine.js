d073fe8265aa855409fbf88e51b4ba726bf6f415
itshuien <huien_wong@hotmail.com>
2021-02-23 10:07:23 +0800

    Create HorizontalLine component

---
+++ src/components/HorizontalLine.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function HorizontalLine() {
+  return (
+    <View style={{
+      height: 1,
+      backgroundColor: '#ddd',
+      marginVertical: 16,
+    }} />
+  )
+}
